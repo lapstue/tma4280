5c7339b1ed7cf77138abbd26d77645f0b3cde139
Peter Holiman <pholi90@gmail.com>
2016-04-07 13:59:23 +0200

    MPI + noen prints

--- poisson.c
+++ poisson.c
@@ -12,12 +12,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <memory.h>
 #include <mpi.h>
-
 
 #define PI 3.14159265358979323846
 #define true 1
 #define false 0
+
+
+
 
 typedef double real;
 typedef int bool;
@@ -32,12 +35,29 @@ void fstinv_(real *v, int *n, real *w, int *nn);
 
 int main(int argc, char **argv)
 {
+
+    int size , rank, number; 
+MPI_Init(&argc, &argv); 
+MPI_Comm_size(MPI_COMM_WORLD , &size);
+MPI_Comm_rank(MPI_COMM_WORLD , &rank);
+
     if (argc < 2) {
         printf("Usage:\n");
         printf("  poisson n\n\n");
         printf("Arguments:\n");
         printf("  n: the problem size (must be a power of 2)\n");
     }
+
+    //int nproc = atoi(argv[2]);
+
+    if(rank == 0){
+        printf("Prosess 1\n");
+    }
+
+    if(rank != 0){
+        printf("Prosess n\n");
+    }
+
 
     // The number of grid points in each direction is n+1
     // The number of degrees of freedom in each direction is n-1
@@ -48,27 +68,41 @@ int main(int argc, char **argv)
 
     // Grid points
     real *grid = mk_1D_array(n+1, false);
+
+    printf("GRID\n");
     for (size_t i = 0; i < n+1; i++) {
         grid[i] = i * h;
+           printf("%f ", grid[i]);
     }
+    printf("\n");
 
-    // The diagonal of the eigenvalue matrix of T
+
+
+        printf("The diagonal of the eigenvalue matrix of T\n");
+
     real *diag = mk_1D_array(m, false);
     for (size_t i = 0; i < m; i++) {
         diag[i] = 2.0 * (1.0 - cos((i+1) * PI / n));
+        printf("%f   ", diag[i]);
     }
 
-    // Initialize the right hand side data
+     printf("\n");
+
+
+    printf(" Initialize the right hand side data \n" );
     real **b = mk_2D_array(m, m, false);
     real **bt = mk_2D_array(m, m, false);
     real *z = mk_1D_array(nn, false);
     for (size_t i = 0; i < m; i++) {
         for (size_t j = 0; j < m; j++) {
             b[i][j] = h * h * rhs(grid[i], grid[j]);
+                    printf("%f   ", b[i][j]);
+
         }
+        printf("\n");
     }
 
-    // Calculate Btilde^T = S^-1 * (S * B)^T
+    printf("  Calculate Btilde^T = S^-1 * (S * B)^T \n Bruker hele den FST-greia");
     for (size_t i = 0; i < m; i++) {
         fst_(b[i], &n, z, &nn);
     }
@@ -77,10 +111,11 @@ int main(int argc, char **argv)
         fstinv_(bt[i], &n, z, &nn);
     }
 
-    // Solve Lambda * Xtilde = Btilde
+    printf(" Solve Lambda * Xtilde = Btilde\n");
     for (size_t i = 0; i < m; i++) {
         for (size_t j = 0; j < m; j++) {
             bt[i][j] = bt[i][j] / (diag[i] + diag[j]);
+            printf("%f   ", bt[i][j]);
         }
     }
 
@@ -101,7 +136,7 @@ int main(int argc, char **argv)
         }
     }
 
-    printf("u_max = %e\n", u_max);
+    printf("u_maximus = %e\n", u_max);
 
     return 0;
 }
@@ -114,6 +149,7 @@ void transpose(real **bt, real **b, size_t m)
 {
     for (size_t i = 0; i < m; i++) {
         for (size_t j = 0; j < m; j++) {
+           
             bt[i][j] = b[j][i];
         }
     }
